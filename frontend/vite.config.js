30740a84e2e8ca650c6355fd8c9a9db6cb547737
Burhaan <burhaanashraf@outlook.com>
2026-04-20 19:54:46 +0530

    added custom domain

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ["ai-resume-analyzer-production-463e.up.railway.app"],
+    allowedHosts: [
+      "ai-resume-analyzer-production-463e.up.railway.app",
+      "ai-resume-analyzer.spacesdrive.cc",
+    ],
   },
   server: {
     proxy: {
