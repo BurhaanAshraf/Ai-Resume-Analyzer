69181bad283f40b400c29fe7300a8bf730963e83
Burhaan <burhaanashraf@outlook.com>
2026-04-20 18:04:56 +0530

    vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ["ai-resume-analyzer-production-463e.up.railway.app"],
+  },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
+      "/api": {
+        target: "http://localhost:5000",
         changeOrigin: true,
       },
     },
